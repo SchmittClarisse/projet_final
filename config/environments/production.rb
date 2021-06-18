e292f38f18aa3cc1536f29d452dd974d3db1914c
SchmittClarisse <82055525+SchmittClarisse@users.noreply.github.com>
2021-06-18 14:49:06 +0200

    mailer correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "https://les-petites-histoires-du-soir.herokuapp.com/", port: 587 }
+  config.action_mailer.default_url_options = { host: "https://les-petites-histoires-du-soir.herokuapp.com/" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "in-v3.mailjet.com",
