aa321c417915b897d687119aff0c4d23cc85f45d
Adri-III <trrs.adrien@gmail.com>
2021-06-24 15:01:33 +0200

    Projet Final - Button Back added to PageShow

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_06_23_204152) do
     t.string "facebook_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
